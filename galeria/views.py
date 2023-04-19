613ea1d25380d52cff694a74b8540179c0fce93d
Nathã Wolff <natha.wolff9@gmail.com>
2023-04-19 12:28:34 +0000

    create database

--- galeria/views.py
+++ galeria/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request,'galeria/index.html')
+
+    dados = {
+        1:{"nome":"Nebulosa de Carina",
+            "legenda":"Nasa"},
+        2:{"nome":"Gamora",
+            "legenda":"Alura"},
+        3:{"nome":"Aurora boreal",
+            "legenda":"irmão urso"},
+        4:{"nome":"luzinha no ceu",
+            "legenda":"discovery"}
+    }
+
+    return render(request,'galeria/index.html', {"cards":dados})
 
 def imagem(request):
     return render(request,'galeria/imagem.html')
